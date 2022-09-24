863472f3e4dc6ad1b06368993953e03ccce2c66e
removedporn <86824510+removedporn@users.noreply.github.com>
2022-09-25 00:07:05 +0800

    Delete mysql directory

--- mysql/create-testing-database.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-mysql --user=root --password="$MYSQL_ROOT_PASSWORD" <<-EOSQL
-    CREATE DATABASE IF NOT EXISTS default;
-    GRANT ALL PRIVILEGES ON default.* TO '$MYSQL_USER'@'%';
-EOSQL
