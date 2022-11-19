701c68498471cc37f29f97e7d5505ffe5e674c74
Andrey Helldar <helldar@dragon-code.pro>
2022-11-19 14:37:56 +0300

    Update create-testing-database.sh

--- mysql/create-testing-database.sh
+++ mysql/create-testing-database.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 mysql --user=root --password="$MYSQL_ROOT_PASSWORD" <<-EOSQL
-    CREATE DATABASE IF NOT EXISTS default;
+    CREATE DATABASE IF NOT EXISTS \`default\`;
     GRANT ALL PRIVILEGES ON \`default%\`.* TO '$MYSQL_USER'@'%';
 EOSQL
