cb8b43ef5b68f295f823a316e9c35533dd18b7d3
Andrey Helldar <helldar@dragon-code.pro>
2024-12-08 17:19:04 +0300

    Removed `reset-users-passwords.sh`

--- mysql/reset-users-passwords.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-mysql --user=root --password="$MYSQL_ROOT_PASSWORD" <<-EOSQL
-    ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY '$MYSQL_ROOT_PASSWORD';
-    ALTER USER '$MYSQL_USER'@'%' IDENTIFIED WITH mysql_native_password BY 'MYSQL_PASSWORD';
-EOSQL
