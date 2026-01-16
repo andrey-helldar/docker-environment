fc4070c751b0220b39c37e3ae92328da166b8e21
Andrey Helldar <helldar@dragon-code.pro>
2026-01-16 13:07:25 +0300

    Limit `docker compose build --pull` to Elasticsearch in update scripts

--- shell/update.ps1
+++ shell/update.ps1
@@ -1,4 +1,4 @@
 docker compose pull
-docker compose build --pull
+docker compose build --pull elasticsearch
 docker compose stop
 docker compose up -d --build
--- shell/update.sh
+++ shell/update.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 docker compose pull
-docker compose build --pull
+docker compose build --pull elasticsearch
 docker compose stop
 docker compose up -d --build
