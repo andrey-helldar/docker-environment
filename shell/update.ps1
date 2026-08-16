080d23bfe604e087763de91e38d727a50f9d682a
Andrey Helldar <helldar@dragon-code.pro>
2026-08-16 13:20:31 +0300

    Added timer

--- shell/update.ps1
+++ shell/update.ps1
@@ -2,3 +2,11 @@ docker compose pull
 docker compose build --pull elasticsearch
 docker compose stop
 docker compose up -d --build
+
+for ($i = 10; $i -gt 0; $i--) {
+    Write-Host "`rClosing in $i... " -NoNewline
+    Start-Sleep -Seconds 1
+}
+
+Write-Host "`rClosing...          "
+exit 0
